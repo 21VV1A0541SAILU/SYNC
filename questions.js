dccf0d6cab255e5ddf35519abf92cc15c0d2f46f
Shanmuka priya pinapathurni <101045793+21VV1A0541SAILU@users.noreply.github.com>
2023-07-23 09:52:38 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,115 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+      numb:3,
+      question: "What is the inline element in the below list?",
+      answer : "li",
+      options: [
+          "li",
+          "p",
+          "td",
+          "h1"
+      ]
+  },
+    {
+      numb:4,
+      question: "What tag used for making the link change the color while moving over it?",
+      answer : "Hover",
+      options: [
+          "Hover",
+          "Active",
+          "Visited",
+          "Linked"
+      ]
+  },
+    {
+      numb:5,
+      question: "Where can we attach the javascript code?",
+      answer : "all of the above",
+      options: [
+         "Head",
+         "body",
+         "external file ",
+         "all of the above"
+     ]
+
+  },
+   {
+     numb: 6,
+     question: "The Tag used for emphasizing the text?",
+     answer: "em",
+     options: [
+       "em",
+       "strong",
+       "i",
+       "b"
+     ]
+   },
+   
+   {
+    numb: 7,
+    question: "The tag which doesn't have closing tag ?",
+    answer: "img",
+    options: [
+      "a",
+      "image",
+      "img",
+      "td"
+    ]
+  },
+  
+  {
+    numb: 8,
+    question: "What is HTML?",
+    answer: "All of the mentioned",
+    options: [
+      "HTML describes the structure of a webpage",
+      "HTML is the standard markup language mainly used to create web pages",
+      "HTML consists of a set of elements that helps the browser how to view the content",
+      "All of the mentioned"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which of the following is used to read an HTML page and render it?",
+    answer: "Web browser",
+    options: [
+      "Web browser",
+      "Web server",
+      "Web network",
+      "Web matrix"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which of the following is not a difference between HTML and XHTML?",
+    answer: "Charset in both html and xhtml is “text/html”",
+    options: [
+      "Tags and attributes are case-insensitive in HTML but not in XHTML",
+      "Charset in both html and xhtml is “text/html”",
+      "Special characters must be escaped using character entities in XHTML unlike HTML",
+      "Charset in html is “text/html” where as in xhtml it is “application/xml+xhtml”"
+    ]
+  },
+];
